66826562ff82b9b56639aca771a501f24d511ec4
pipisasa <emir0725mk@gmail.com>
2024-05-23 01:38:47 +0600

    add README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# @pipisasa/storage - Abstraction for Storage implementations
+
+Such as `LocalStorage`, `SessionStorage`, `Cookie` ext.
+
+Example
+
+```ts
+// utils/storage.ts
+
+import { Storage } from "@pipisasa/storage";
+import { SerializableLocalStorage } from "@pipisasa/storage/localStorage";
+
+// Later if you want to move on form localStorage to another type of storage you can just replace this variable
+export const storage: Storage = new SerializableLocalStorage("some-prefix:");
+```
--- mod.ts
+++ mod.ts
@@ -1,1 +1,18 @@
+/**
+ * # @pipisasa/storage - Abstraction for Storage implementations
+ * 
+ * Such as `LocalStorage`, `SessionStorage`, `Cookie` ext.
+ * 
+ * Example
+ * 
+ * ```ts
+ * // utils/storage.ts
+ * 
+ * import { Storage } from "@pipisasa/storage";
+ * import { SerializableLocalStorage } from "@pipisasa/storage/localStorage";
+ * 
+ * // Later if you want to move on form localStorage to another type of storage you can just replace this variable
+ * export const storage: Storage = new SerializableLocalStorage("some-prefix:");
+ * ```
+ */
 export * from './abstraction.ts';
